51dff109b4c81d0de9b4a074f2b9b1221878b297
James Burnell <jamesburnell@Jamess-MacBook-Pro.local>
2016-03-01 15:28:36 -0800

    login page up, initial login routes work. now need to implement react router

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -3,9 +3,32 @@ var React = require('react');
 var Login = React.createClass({
 	render(){
 		return (
-			<div>Login, fool!</div>
+			<div className="uk-vertical-align-middle" style={styles.formContainer}>
+				<h1>butterfli.</h1>
+				<form className="uk-panel uk-panel-box uk-form">
+					
+					<div className="uk-form-row">
+						<input ref="username" type="text" placeholder="Username" />
+					</div>
+					<div className="uk-form-row">
+						<input ref="password" type="text" placeholder="Password" />
+					</div>
+					<div className="uk-form-row">
+						<a onClick={function(){this.props.updateCreds(this.refs.username.value, this.refs.password.value)}.bind(this)} className="uk-width-1-1 uk-button uk-button-primary uk-button-large"></a>
+					</div>
+					<div className="uk-form-row"></div>
+
+				</form>
+
+			</div>
 		)
 	}
 });
+
+var styles = {
+	formContainer: {
+		width: 250
+	}
+}
 
 module.exports = Login;
--- client/main.jsx
+++ client/main.jsx
@@ -1,24 +1,55 @@
 var React = require('react'),
-	ReactDOM = require('react-dom');
-var Login = require('./components/Login.jsx');
+	ReactDOM = require('react-dom'),
+    Login = require('./components/Login.jsx'),
+    AccountHome = require('./components/AccountHome.jsx');
 
 var Butterfli = React.createClass({
 
-	getInitialState(){
+	getInitialState: function(){
 		return {
-			isLoggedIn: true
+			isLoggedIn: false,
+			username: null,
+			password: null
 		}
 	},
 
-	render: function () {
+	// save the inputted username and password
+	updateCreds: function(username, password){
 
+		// promise to be sure the state is set before attempting the login request
+		new Promise(function(resolve, reject){
+			this.setState({
+				username: username,
+				password: password
+			})
+			resolve(this.state.password)
+		}.bind(this)).then(function(value) {
+
+			// send login request
+			this.checkCreds();
+		}.bind(this));
+	},
+
+	// make request to log the user in
+	checkCreds: function (){
+		console.log('username: ', this.state.username, 'password: ', this.state.password);
+		this.setState({
+			isLoggedIn: true
+		})
+
+	},
+
+	render: function (){
+
+		// check if logged in, render appropriate component
 		var loginRoute = function () {
 			if(this.state.isLoggedIn) {
-				return (<div>Hello World, fool!</div>)
+				return (<AccountHome username={this.state.username} />)
 			} else if (!this.state.isLoggedIn) {
-				return (<Login />)
+				return (<Login updateCreds={this.updateCreds} />)
 			}
 		}.bind(this)
+
 
 		return (
 			<div>
